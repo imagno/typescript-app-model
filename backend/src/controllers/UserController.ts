514ea15314b793dfc2335cf548f606f575e4dfa8
imagno <igor.magno@protonmail.com>
2022-01-16 15:02:36 -0300

    aplicando conceitos de typescript

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import EmailService from "../services/EmailService";
 
 const users = [
   { name: 'imagno', email: 'imagno@email.com.br' },
@@ -7,5 +8,16 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail(
+      { name: 'imagno', email: 'imagno@email.com.br' },
+      { subject: 'Bem-vindo', body: 'Seja bem-vindo ao sistema!' }
+    );
+
+    return res.send('Usuário criado com sucesso!');
   }
 };
