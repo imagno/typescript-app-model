544df0da3475d246c99ead9461a4d2774c9153c8
imagno <igor.magno@protonmail.com>
2022-01-16 20:05:57 -0300

    tipando as propriedades dos componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import api from './services/api';
 
+import User from './components/User';
 interface IUser {
   name: string;
   email: string;
@@ -17,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      { users.map(user => <p>{user.name}</p>) }
+      { users.map(user => <User key={user.email} user={user} />) }
     </div>
   );
 }
