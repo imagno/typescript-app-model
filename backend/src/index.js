43c44911f0bd001111c89512815e68d2324b9027
imagno <igor.magno@protonmail.com>
2022-01-16 10:04:54 -0300

    rebuild utilizando tsconfig

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var express = require("express");
-var app = express();
+var express_1 = require("express");
+var app = (0, express_1["default"])();
 app.get('/', function (req, res) {
     return res.send('Hello World');
 });
